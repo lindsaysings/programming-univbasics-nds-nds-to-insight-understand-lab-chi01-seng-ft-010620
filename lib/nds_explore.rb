6f88f0d814bdaf855f5c01d1ac8a8b920aae44ce
Lindsay M <lindsaymecher@gmail.com>
2019-12-31 06:47:44 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,9 +3,16 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
+  pp nds
   nil
 end
 
 def print_first_directors_movie_titles
 end
+
+=begin
+'When exploring the data structure using custom methods,' do
+  it 'print_first_directors_movie_titles loops over Spielberg’s movies and prints out their names with newlines' do
+    expect { print_first_directors_movie_titles }.to output("Jaws\nClose Encounters of the Third Kind\nRaiders of the Lost Ark\nE.T. the Extra-terrestrial\nSchindler's List\nLincoln\n"
+    
+=end
